f50f15d45d962f92f32150ef0d68b7365643a688
Andrea J <andreaewilliams@yahoo.com>
2020-02-04 23:30:44 -0500

    Revert "Revert "Revert "Update processingGradesv3.py"""
    This reverts commit 2bac37d9a5cb63869c807aa3eb05a3a0d50c0cc0.

--- codeThatNeedsWork/processingGradesv3.py
+++ codeThatNeedsWork/processingGradesv3.py
@@ -1,37 +1,23 @@
-def average(testGrades,hwGrades,projGrades,labGrades):
-  totalTest = 0
-  totalHW = 0
-  totalProj = 0
-  totalLab = 0
+#this function can be dramatically shortened by using 
+#just one for loop and taking one parameter, it could also return 
+#the average which would make the code easier to work with
+def average(grade_list):#only asks for one list
+  total = 0
   
-  #TEST GRADES AVERAGE
-  for elem in testGrades:
-    if elem != "tests":
-      totalTest = totalTest + elem
-  testAverage = totalTest/(len(testGrades)-1)
+  #Caluclates average for referenced list
+  for elem in grade_list:
+    if str(elem).isdigit():#if the element as a string is a digit
+      total = total + elem
+  testAverage = total/(len(grade_list)-1)
   
-  #HOMEWORK GRADES AVERAGE
-  for elem in hwGrades:
-    if elem != "homework":
-      totalHW = totalHW + elem
-  hwAverage = totalHW/(len(hwGrades)-1)
-
-  #PROJECT GRADES AVERAGE
-  for elem in projGrades:
-    if elem != "projects":
-      totalProj = totalProj + elem
-  projAverage = totalProj/(len(projGrades)-1)
-
-  #LAB GRADES AVERAGE
-  for elem in labGrades:
-    if elem != "labs":
-      totalLab = totalLab + elem
-  labAverage = totalLab/(len(labGrades)-1)
 
  #Weighted Average
   
   weightedAverage(testAverage,hwAverage, projAverage, labAverage)
 
+#i would do something similar to this one
+#making it handle one list at a time passing
+#the list and the weight as parameters
 def weightedAverage(testAverage,hwAverage, projAverage, labAverage):
   weightedTest = testAverage * 0.3
   weightedHW = hwAverage * 0.2
